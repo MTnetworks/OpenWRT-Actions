107fb95bb77997f889e7cc441cb36addfa97f959
MTnetworks <mtwl520@gmail.com>
2025-12-11 09:10:13 +0800

    Initial project import

---
+++ README.md
@@ -0,0 +1,25 @@
+# OpenWRT-Actions
+
+## 使用方法
+
+fork这个仓库。可选编译immortalwrt或者openwrt。可以上传.config也可以在编译的时候勾选ssh选项手动配置config。还可以将已有的openwrt的/etc下的配置文件放入files目录，编译的时候会自动使用配置文件，这样更新固件就不会丢失配置了。但是将项目私有化会导致编译环境优化的存储空间只有50G+，大概率会报空间不足的错误，公开项目就能使用120G存储空间。
+
+    勾选了很多插件容易导致编译慢以及报错，建议按需勾选。
+    cd openWRT
+    make menuconfig
+
+## 默认.config如下
+
+- target x86/x86_64
+- 160M 内核空间
+- 1600M root空间
+  
+-------------------------------
+
+- network/adguardhome
+- luci-app-homeproxy
+- luci-app-openclash
+- luci-app-acme
+- luci-app-wol
+
+  
---
+++ diy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#=================================================
+# Lisence: MIT
+# Author: MTnetwork
+# Blog: https://www.mtwl.net
+#=================================================
+# Add a feed source
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+
+# Modify default IP
+#修改默认 IP为192.168.1.1
+sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+# 修改机器名称
+sed -i 's/OpenWrt/MTnetwork/g' package/base-files/files/bin/config_generate
+# 编辑默认的luci显示的固件名称
+sed -i 's/OpenWrt/MTopenwrt/g' package/base-files/files/bin/config_generate
+#删除原主题	
+rm -rf package/lean/luci-theme-netgear
+#添加新的主题和插件
+git clone https://github.com/sypopo/luci-theme-atmaterial.git package/lean/luci-theme-atmaterial
+git clone https://github.com/vernesong/OpenClash.git package/lean/luci-app-OpenClash
+# '修改默认主题'
+# sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
+#取消bootstrap为默认主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+#删除默认密码
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+# 修改wifi名称
+sed -i 's/OpenWrt/MTnetwork/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#修改banner
+rm -rf package/base-files/files/etc/banner
+cp -f ../banner package/base-files/files/etc/
+
+# 添加额外的软件包，echo 方式和git clone 方式二选一即可
+#echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+#git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+#git clone https://github.com/kenzok8/small.git package/small
+git clone https://github.com/stevenjoezhang/luci-app-adguardhome.git package/ADGH
