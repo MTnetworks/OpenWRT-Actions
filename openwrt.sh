ef9eb5848c40260acd66ac6ce9a2810c77651940
MTnetworks <61041491+MTnetworks@users.noreply.github.com>
2025-12-12 16:41:55 +0800

    Configure OpenWrt with custom settings and themes
    This script adds additional software package feeds, modifies default settings such as IP address, device name, and Wi-Fi name, and includes new themes and plugins for OpenWrt.

---
+++ openwrt.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#=================================================
+# Lisence: MIT
+# Author: MTnetwork
+# Blog: https://www.mtwl.net
+#=================================================
+# # 添加额外的软件包Add a feed source
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git lean_packages https://github.com/coolsnowwolf/packages.git' >>feeds.conf.default
+# Modify default IP
+#修改默认 IP为192.168.1.1
+sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+# 修改机器名称
+sed -i 's/OpenWrt/MTnetwork/g' package/base-files/files/bin/config_generate
+# 编辑默认的luci显示的固件名称
+sed -i 's/OpenWrt/MTopenwrt/g' package/base-files/files/bin/config_generate
+#添加新的主题和插件
+git clone https://github.com/sypopo/luci-theme-atmaterial.git package/luci-theme-atmaterial
+# Argon主题配置
+git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+# Argon主题
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+# Edge主题
+git clone https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
+# PassWall2
+git clone https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
+# 
+# '修改默认主题'
+# sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
+#删除默认密码
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+# 修改wifi名称
+sed -i 's/OpenWrt/MTnetwork/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#修改banner
+rm -rf package/base-files/files/etc/banner
+cp -f ../banner package/base-files/files/etc/
+
